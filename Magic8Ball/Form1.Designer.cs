4163f8b9b44411ccda37a860a78cfa78aa21770a
quinstev819 <quinstev819@ed.amdsb.ca>
2019-10-21 13:33:09 -0400

    Add project files.

---
+++ Magic8Ball/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Magic8Ball
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.questionLabel = new System.Windows.Forms.Label();
+            this.button = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // questionLabel
+            // 
+            this.questionLabel.AutoSize = true;
+            this.questionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.questionLabel.ForeColor = System.Drawing.Color.White;
+            this.questionLabel.Location = new System.Drawing.Point(74, 29);
+            this.questionLabel.Name = "questionLabel";
+            this.questionLabel.Size = new System.Drawing.Size(240, 29);
+            this.questionLabel.TabIndex = 1;
+            this.questionLabel.Text = "Think of a Question";
+            // 
+            // button
+            // 
+            this.button.BackColor = System.Drawing.Color.Transparent;
+            this.button.BackgroundImage = global::Magic8Ball.Properties.Resources.clickBall;
+            this.button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button.FlatAppearance.BorderSize = 0;
+            this.button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button.Location = new System.Drawing.Point(28, 43);
+            this.button.Margin = new System.Windows.Forms.Padding(0);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(315, 275);
+            this.button.TabIndex = 0;
+            this.button.UseVisualStyleBackColor = false;
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(12, 318);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(360, 23);
+            this.outputLabel.TabIndex = 2;
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.questionLabel);
+            this.Controls.Add(this.button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Label questionLabel;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ Magic8Ball/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Magic8Ball
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Random randGen = new Random();
+            int answers = randGen.Next(1, 21);
+            
+            switch (answers)
+            {
+                case 1:
+                    outputLabel.Text = "It is certain.";
+                    break;
+                case 2:
+                    outputLabel.Text = "It is decidedly so.";
+                    break;
+                case 3:
+                    outputLabel.Text = "Without a doubt.";
+                    break;
+                case 4:
+                    outputLabel.Text = "Yes - definitely.";
+                    break;
+                case 5:
+                    outputLabel.Text = "You may rely on it.";
+                    break;
+                case 6:
+                    outputLabel.Text = "As I see it, yes.";
+                    break;
+
+            }
+        }
+    }
+}
